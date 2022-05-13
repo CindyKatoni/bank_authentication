c95951779ec9619fec0f0c1565e5609a3cc996e4
Muema <KEN20952@kcbgroup.com>
2022-05-13 16:58:15 +0300

    Database configuration.

---
+++ src/main/java/com/example/bank_authentication/BankAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAuthenticationApplication.class, args);
+    }
+
+}
