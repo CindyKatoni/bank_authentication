9eadac78b2245c8b9b7d7823800f376de28f9167
Muema <KEN20952@kcbgroup.com>
2022-05-16 10:13:33 +0300

    Debug dependencies. Edit database name in the application properties file. Create a User model for registration.

---
+++ src/main/java/com/example/bank_authentication/model/User.java
@@ -0,0 +1,46 @@
+package com.example.bank_authentication.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    //AUTO is used in MySQL Database; INCREMENT is used in SQL-server
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 20)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
